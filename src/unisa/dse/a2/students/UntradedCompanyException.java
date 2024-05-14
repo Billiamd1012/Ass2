e3a2986b78f5db61c833f426da36ee0cb49b25b5
Billiamd1012 <william.darker2003@gmail.com>
2024-05-14 22:28:46 +1000

    Add UntradedEexception

--- src/unisa/dse/a2/students/UntradedCompanyException.java
+++ src/unisa/dse/a2/students/UntradedCompanyException.java
@@ -4,5 +4,7 @@ public class UntradedCompanyException
 {
 	public UntradedCompanyException(String companyCode)
 	{
+		String exception = companyCode + "is not a listed company on this exchange";
+		System.err.println(exception);
 	}
 }
